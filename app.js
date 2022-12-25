231d5c686073f16c8b38a811757b5bc256fbfb80
EC2 Default User <ec2-user@ip-172-31-38-76.ap-northeast-1.compute.internal>
2022-12-25 10:34:10 +0000

    2022/12/25

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-alert('hello world');
+let hello = 'hello world';
+let max = 100;
+let number = 10;
+let count = 0;
+
+let i;
+let num = 0;
+ for(i = 1;i < 11;i++){
+     num = num + i;
+ }
+
+ alert(max + number)
+ alert(max - number)
+ alert(max * number)
+  alert( max / number)
