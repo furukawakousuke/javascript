4f50c71da8722320e3a997d6027a70e2c272f8a5
EC2 Default User <ec2-user@ip-172-31-38-76.ap-northeast-1.compute.internal>
2022-12-27 08:47:43 +0000

    2022/12/27

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-let hello = 'hello world';
-let max = 100;
-let number = 10;
-let count = 0;
+//let alertString;
 
-let i;
-let num = 0;
- for(i = 1;i < 11;i++){
-     num = num + i;
- }
+//alertString = addString('WebCamp');
 
- alert(max + number)
- alert(max - number)
- alert(max * number)
-  alert( max / number)
+//alert(alertString);
+
+//function addString(strA){
+//  let addStr = "Hello " + strA;
+//  return addStr;
+//}
+
+let promptStr = prompt('何か好きな文字を入力してください。')
+
+alert(promptStr);
